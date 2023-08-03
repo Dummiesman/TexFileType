9230c0a0fe10b98fedd951c7306fea85dc86ace5
Dummiesman <aaro4130@gmail.com>
2023-08-03 12:01:35 -0400

    Add git repository as URI in PluginSupportInfo

--- PluginSupportInfo.cs
+++ PluginSupportInfo.cs
@@ -25,6 +25,6 @@ namespace TexFileType
 
         public Version Version => typeof(PluginSupportInfo).Assembly.GetName().Version;
 
-        public Uri WebsiteUri => null;
+        public Uri WebsiteUri => new("https://github.com/Dummiesman/TEXFileFormat");
     }
 }
